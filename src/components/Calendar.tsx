9f4980d3beec85fad01d44ac1877845f7d8851e7
LeeMoonki <shinbaek89@gmail.com>
2020-05-11 09:18:34 +0900

    캘린더 형태 및 기초 기능 잡기

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Dimensions, ScrollView, StyleSheet, Text, View } from 'react-native';
 import { tdate } from '@shinbaek/tool-case';
+
+const WINDOW_WIDTH = Dimensions.get('window').width;
 
 const shape = (dates: DateStructure[]): DateStructure[][] => {
   const ds = dates.slice();
@@ -26,19 +28,71 @@ function Calendar() {
     setDates(shape(tdate.getDatesOfMonth(year, month, false)));
   }, [month]);
 
+  const onPressPreviousMonth = () => {
+    if (month === 1) {
+      setYear(year - 1);
+      setMonth(12);
+    } else {
+      setMonth(month - 1);
+    }
+  };
+
+  const onPressNextMonth = () => {
+    if (month === 12) {
+      setYear(year + 1);
+      setMonth(1);
+    } else {
+      setMonth(month + 1);
+    }
+  };
+
   return (
-    <View>
-      <Text>Calendar</Text>
-      <View>
-        <View style={styles.calendar_wrapper}></View>
+    <ScrollView>
+      <View style={styles.calendar_header}>
+        <Button title="<" onPress={onPressPreviousMonth} />
+        <Text>{`${year}.${month}`}</Text>
+        <Button title=">" onPress={onPressNextMonth} />
       </View>
-    </View>
+      <View>
+        <View style={styles.calendar_wrapper}>
+          {dates.map(week => {
+            return week.map(date => {
+              return (
+                <View key={`${date.year}${date.month}${date.date}`} style={styles.date_card}>
+                  <Text>{date.date}</Text>
+                </View>
+              );
+            });
+          })}
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  calendar_header: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
   calendar_wrapper: {
-    
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    flexWrap: 'wrap',
+    minWidth: WINDOW_WIDTH,
+    maxWidth: WINDOW_WIDTH,
+    width: WINDOW_WIDTH
+  },
+  date_card: {
+    marginTop: 5,
+    width: WINDOW_WIDTH * 0.13,
+    height: WINDOW_WIDTH * 0.13,
+    borderWidth: 1,
+    borderColor: '#f00',
+    borderRadius: 5,
   }
 });
 
